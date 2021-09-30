f031ff9f65b47f6f29a9a082a1cd31cf140380ac
pupil <pupil@ivkhk.ee>
2021-09-30 10:34:14 +0300

    final commit

---
+++ src/jktv20_controllwork_javahello/JKTV20_ControllWork_JavaHello.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jktv20_controllwork_javahello;
+
+/**
+ *
+ * @author pupil
+ */
+public class JKTV20_ControllWork_JavaHello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Java Hello!");
+    }
+    
+}
+
+
